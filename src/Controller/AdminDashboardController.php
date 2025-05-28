ba55cff6e07827823414bbf114ce7f680c2b87d0
AmrSlama <amrdroidcommunity@gmail.com>
2025-05-28 11:47:34 +0100

    changing some security settings

--- src/Controller/AdminDashboardController.php
+++ src/Controller/AdminDashboardController.php
@@ -11,12 +11,6 @@ final class AdminDashboardController extends AbstractController
     #[Route('/admin/dashboard', name: 'app_admin_dashboard')]
     public function index(): Response
     {
-        // Check if the user has ROLE_ADMIN
-        if (!$this->isGranted('ROLE_ADMIN')) {
-            // Redirect to login page if they don't have the required role
-            return $this->redirectToRoute('app_login');
-        }
-
         return $this->render('admin_dashboard/index.html.twig', [
             'controller_name' => 'AdminDashboardController',
         ]);
