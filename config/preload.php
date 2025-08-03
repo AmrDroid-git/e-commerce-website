541bbd2697597e514705de7ea8f6832a430d4dc7
AmrSlama <amrdroidcommunity@gmail.com>
2025-08-03 01:09:18 +0100

    Update preload.php for 2025-08-03

--- config/preload.php
+++ config/preload.php
@@ -3,3 +3,5 @@
 if (file_exists(dirname(__DIR__).'/var/cache/prod/App_KernelProdContainer.preload.php')) {
     require dirname(__DIR__).'/var/cache/prod/App_KernelProdContainer.preload.php';
 }
+
+# backdated-commit: 2025-08-03 00:00:00
