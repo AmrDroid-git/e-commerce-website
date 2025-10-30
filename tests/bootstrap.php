44739345feae6e455d9f0798c8c2ce2da86107e1
AmrSlama <amrdroidcommunity@gmail.com>
2025-10-30 16:43:14 +0100

    Update bootstrap.php for 2025-10-30

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,3 +7,5 @@ require dirname(__DIR__).'/vendor/autoload.php';
 if (method_exists(Dotenv::class, 'bootEnv')) {
     (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
 }
+
+# backdated-commit: 2025-10-30 00:00:00
